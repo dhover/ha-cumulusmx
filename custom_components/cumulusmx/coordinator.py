4f30bd7a78c3c23af507f802dc5b4e25f15eb73b
Dinant <dinant@dtwm.nl>
2025-08-19 17:38:53 +0200

    Refactor CumulusMXCoordinator to prioritize options over data for configuration parameters

--- custom_components/cumulusmx/coordinator.py
+++ custom_components/cumulusmx/coordinator.py
@@ -27,17 +27,16 @@ _LOGGER = logging.getLogger(__name__)
 class CumulusMXCoordinator(DataUpdateCoordinator):
     def __init__(self, hass: HomeAssistant, config_entry: ConfigEntry):
         self.hass = hass
-        self.host = config_entry.data[CONF_HOST]
-        self.port = config_entry.data[CONF_PORT]
+        self.host = config_entry.options.get(CONF_HOST, config_entry.data.get(CONF_HOST))
+        self.port = config_entry.options.get(CONF_PORT, config_entry.data.get(CONF_PORT))
         self.url = SENSOR_API_URL.format(host=self.host, port=self.port)
-        webtags = config_entry.data.get(CONF_WEBTAGS, "temp,hum,dew")
+        webtags = config_entry.options.get(CONF_WEBTAGS, config_entry.data.get(CONF_WEBTAGS))
         self.post_body = create_sensor_post_body(webtags)
         _LOGGER.debug("Send to CumulusMX: %s", self.post_body)
-        update_interval = timedelta(seconds=config_entry.data.get(
-            CONF_UPDATE_INTERVAL, DEFAULT_UPDATE_INTERVAL))
+        update_interval = timedelta(seconds=config_entry.options.get(
+            CONF_UPDATE_INTERVAL, config_entry.data.get(CONF_UPDATE_INTERVAL, DEFAULT_UPDATE_INTERVAL)))
 
-        self.api = CumulusMXApi(self.hass, self.url,
-                                self.post_body)  # <-- Instantiate API
+        self.api = CumulusMXApi(self.hass, self.url, self.post_body)
 
         super().__init__(
             hass,
